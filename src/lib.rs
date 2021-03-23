53cf0d193909fb160c31a9a9f0f2dc0021e7fa65
Darren Tsung <darrentsung@DarrenTcomputer.attlocal.net>
2021-03-22 20:47:24 -0700

    Add another struct that gives an error

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,8 @@ use rkyv::*;
 
 #[derive(Archive, Debug, Clone, Deserialize, Serialize, PartialEq)]
 pub struct Foo(u32);
+
+#[derive(Archive, Debug, Clone, Deserialize, Serialize, PartialEq)]
+pub struct Bar {
+    id: u32,
+}
