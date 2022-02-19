60533a2ae59777d8bb3e24259c67ae1a50fafdbb
Darren Tsung <darren.tsung@gmail.com>
2022-02-19 15:51:15 -0800

    upload a different issue, trying to serialize vec![Uuid]

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,12 @@
 use rkyv::*;
+use uuid::*;
 
-macro_rules! generate_enums {
-    ($($enum_name:ident$(,)*)*) => {
-        $(
-            #[derive(Archive)]
-            pub enum $enum_name {
-                HasInput,
-            }
-        )*
-    };
-}
-
-generate_enums!(Foo, Baz);
-
-#[derive(Archive)]
-pub enum Bar {
-    HasInput,
+pub fn foo() {
+    let mut serializer = rkyv::ser::serializers::WriteSerializer::new(vec![]);
+    Uuid::new_v4()
+        .serialize(&mut serializer)
+        .expect("can serialize");
+    vec![Uuid::new_v4()]
+        .serialize(&mut serializer)
+        .expect("can serialize");
 }
